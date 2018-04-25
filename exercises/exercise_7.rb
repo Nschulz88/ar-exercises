47e7d5e31634bf27e65e0809ef3759a0d0bb1b7f
Nschulz88 <natalie.schulz@me.com>
2018-04-25 02:54:11 +0000

    Working w user input and errors, ex7 completed

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Pick a store name (name is the city the store is located in): "
+user_pick = gets.chomp
+
+# Store.create(name: user_pick, annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+user1_store = Store.create(name: user_pick)
+
+puts "You picked #{user_pick}"
+
+if user1_store.valid?
+    puts "Your requested store was created."
+else 
+    puts "Your requested store was not created due to this error: #{user1_store.errors.messages}."
+end
+
+
