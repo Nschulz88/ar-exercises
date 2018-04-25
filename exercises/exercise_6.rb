a8247405ecd4b80199eb75923782555eea3e9eaa
Nschulz88 <natalie.schulz@me.com>
2018-04-25 02:19:41 +0000

    Populated employees database, ex6 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jessica", last_name: "Hofchen", hourly_rate: 50)
+@store1.employees.create(first_name: "Natalie", last_name: "Schulz", hourly_rate: 45)
+@store1.employees.create(first_name: "Sarah", last_name: "Schulz", hourly_rate: 45)
+@store2.employees.create(first_name: "Spencer", last_name: "Zaharko", hourly_rate: 55)
+@store2.employees.create(first_name: "Max", last_name: "Muster", hourly_rate: 20)
+@store2.employees.create(first_name: "Bob", last_name: "Steele", hourly_rate: 65)
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 35)
+
